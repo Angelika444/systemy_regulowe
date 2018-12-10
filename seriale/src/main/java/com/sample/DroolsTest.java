fb29eb2baf8f8fc5e68c3d95925a6c8576127e22
unknown <angela.s@onet.eu>
2018-12-10 23:49:44 +0100

    reguly

--- seriale/src/main/java/com/sample/DroolsTest.java
+++ seriale/src/main/java/com/sample/DroolsTest.java
@@ -3,10 +3,11 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
-/**
- * This is a sample class to launch a rule.
- */
+
 public class DroolsTest {
 
     public static final void main(String[] args) {
@@ -16,85 +17,76 @@ public class DroolsTest {
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
             
-            
-            Zapytanie zapytanie=new Zapytanie();
-            zapytanie.setMessage("pregnant?");
-            zapytanie.setStatus(Zapytanie.START);
-            kSession.insert(zapytanie);
-            
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
+    public static class Fact{
+    	private String name;
+        private String answer;
+        
+    	public Fact(String name, String answer) {
+        	this.name=name;
+            this.answer=answer;
+        }
+
+		public String getAnswer() {
+			return answer;
+		}
+
+		public void setAnswer(String answer) {
+			this.answer = answer;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+    	
+    }
     
-    public static class Zapytanie {
-
-    	public static final int YES = 0;
-        public static final int NO = 1;
-        public static final int OTHER =2;
-        public static final int OTHER2 =3;
-        public static final int START =4;
-
-        private String message;
-        private int status;
-        private int number;
-        private int noMoreQuestions=0;
-        private Scanner input;
-        private String last="";
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-        public int getNoMoreQuestions() {
-            return this.noMoreQuestions;
-        }
-
-        public void setNoMoreQuestions(int noMoreQuestions) {
-            this.noMoreQuestions = noMoreQuestions;
-        }
-        public int getNumber() {
-            return this.number;
-        }
-
-        public void setNumber(int number) {
-            this.number = number;
-        }
-        public String getLast() {
-            return this.last;
-        }
-
-        public void setLast(String last) {
-            this.last = last;
-        }
-        public void readNumber() {
-        	input = new Scanner(System.in);
-    		String text = input.nextLine();
-    		try
-    		{
-    			this.number = Integer.parseInt( text); 
-    		}
-    		catch(NumberFormatException e)
-    		{
-    		System.out.println("Tu nie ma liczby :[");
-    		}
-    		
+    
+    public static class Question {
+        
+    	private String question;
+    	private static ArrayList<String> answers = new ArrayList<String>();
+        private String userAnswer="Yes";
+        
+        public Question(String q) {
+        	this.question = q;
         }
         
-   
+        public void showQuestion() {
+			//czeka na odpowiedz uzytkownika, ktora wstawia do userAnswer
+		}
+        
+        public void addAns(String a) {
+			Question.answers.add(a);
+		}
+        
+		public static ArrayList<String> getAnswers() {
+			return answers;
+		}
+		public static void setAnswers(ArrayList<String> answers) {
+			Question.answers = answers;
+		}
+		public String getQuestion() {
+			return question;
+		}
+		public void setQuestion(String question) {
+			this.question = question;
+		}
+
+		public String getUserAnswer() {
+			return userAnswer;
+		}
+
+		public void setUserAnswer(String userAnswer) {
+			this.userAnswer = userAnswer;
+		}
     }
-
-
 }
